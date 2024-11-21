491c63f6498801ae98719fd4e9f599425f5cb76a
ManuelNevado <nevadofabianmanuel@gmail.com>
2024-11-21 13:39:48 +0100

    feat: create repo

---
+++ CMakeFiles/pdftoolssplit.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/PruebaC/PdfTools_Split_C/pdftoolssplit.c" "CMakeFiles/pdftoolssplit.dir/pdftoolssplit.c.o" "gcc" "CMakeFiles/pdftoolssplit.dir/pdftoolssplit.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/pdftoolssplit.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pdftoolssplit.dir/pdftoolssplit.c.o"
+  "CMakeFiles/pdftoolssplit.dir/pdftoolssplit.c.o.d"
+  "pdftoolssplit"
+  "pdftoolssplit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pdftoolssplit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
